e6d4575f94588ea4b36c8e8112fc214e92785211
Olimpia Spyra <olimpia.spyra@gmail.com>
2022-12-16 21:02:12 +0100

    Add folders for pages

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,10 @@
 import React, {Fragment} from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import Home from '../components/pages/Home';
-import Subjects from '../components/pages/Subjects';
-import About from '../components/pages/About';
-import NotFound from '../components/pages/NotFound';
+import Home from '../components/pages/Home/Home';
+import Subjects from '../components/pages/Subjects/Subjects';
+import About from '../components/pages/About/About';
+import NotFound from '../components/pages/NotFound/NotFound';
 import NavbarComponent from '../components/layout/NavbarComponent/NavbarComponentContainer';
 
 const AppRouter = () => {
